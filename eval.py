0ddc08bab9ee80b7db383ffc7a08ab420ab44902
3t <1489272013@qq.com>
2020-02-13 23:19:40 +0800

    Optimize `eval.py`
    - add the option to save the json file when evaluating
    
    - add the option to specify the confidence threshold
    
    - now the program is able to `guess` the path of the saved model
    
    - remove the entry of `fixed seed`
    
    - remove unused import packages

--- eval.py
+++ eval.py
@@ -2,23 +2,14 @@
 import argparse
 import os
 import logging
-import time
-import warnings
-import numpy as np
 import mxnet as mx
-from mxnet import nd
 from mxnet import gluon
-from mxnet import autograd
-import gluoncv as gcv
-from gluoncv import data as gdata
-from gluoncv import utils as gutils
 from gluoncv.model_zoo import get_model
 from gluoncv.data.batchify import Tuple, Stack, Pad
 from gluoncv.data.transforms.presets.yolo import YOLO3DefaultValTransform
 from gluoncv.data.mscoco.detection import COCODetection
 from gluoncv.utils.metrics.coco_detection import COCODetectionMetric
 from tqdm import tqdm
-from gluoncv.data import batchify
 
 
 def parse_args():
@@ -27,7 +18,7 @@ def parse_args():
                         help="Input data shape for evaluation, use 320, 416, 608... ")
     parser.add_argument('--batch-size', type=int, default=1,
                         help='Training mini-batch size. Values higher than 1 may cause problems.')
-    parser.add_argument('--save-prefix', type=str, default='./tiny_result/',
+    parser.add_argument('--save-prefix', type=str, default='./results/',
                         help='Saving parameter prefix')
     parser.add_argument('--dataset', type=str, default='coco',
                         help='Training dataset. Only COCO is supported.')
@@ -37,40 +28,45 @@ def parse_args():
                         help='Eval with GPUs. We recommend using only 1 GPU to eval')
     parser.add_argument('--resume', type=str, default='',
                         help='The path of the saved params')
-    parser.add_argument('--start-epoch', type=int, default=240,
+    parser.add_argument('--start-epoch', type=int, default=-1,
                         help='The epoch of saved parameters')
-    parser.add_argument('--seed', type=int, default=233,
-                        help='Random seed to be fixed.')
+    parser.add_argument('--save-json', action='store_true',
+                        help='To save the detection result to json files')
+    parser.add_argument('--score-thresh', type=float, default=0.001,
+                        help='Detections will be ignored if confidence scores < threshold.')
     return parser.parse_args()
 
 
 def get_dataset(dataset, args):
+    width, height = args.data_shape, args.data_shape
     if dataset.lower() == 'coco':
         val_dataset = COCODetection(root='./data/coco', splits='instances_val2017', skip_empty=False)
-        val_metric = COCODetectionMetric(
-            val_dataset, args.save_prefix + '_eval', cleanup=True,
-            data_shape=(args.data_shape, args.data_shape))
+        val_metric = COCODetectionMetric(val_dataset, args.save_prefix + '_eval', cleanup=not args.save_json,
+                                         data_shape=(height, width), score_thresh=args.score_thresh)
     else:
         raise NotImplementedError('Dataset: {} not implemented.'.format(dataset))
     return val_dataset, val_metric
 
 
-def get_dataloader(net, val_dataset, data_shape, batch_size, num_workers, args):
+def get_dataloader(val_dataset, data_shape, batch_size, num_workers, args):
     """Get dataloader."""
     width, height = data_shape, data_shape
     batchify_fn = Tuple(Stack(), Pad(pad_val=-1))
     val_loader = gluon.data.DataLoader(
         val_dataset.transform(YOLO3DefaultValTransform(width, height)),
-        batch_size, False, last_batch='keep', num_workers=num_workers, batchify_fn=batchify_fn, )
+        batch_size, False, last_batch='keep', num_workers=num_workers, batchify_fn=batchify_fn)
     return val_loader
 
 
 def validate(net, val_data, ctx, eval_metric, size, args):
     """Test on validation dataset."""
+    net.collect_params().reset_ctx(ctx)
     eval_metric.reset()
+    net.hybridize()
     # set nms threshold and topk constraint
     net.set_nms(nms_thresh=0.45, nms_topk=400)
 
+    mx.nd.waitall()
     with tqdm(total=size, ncols=0) as pbar:
         for ib, batch in enumerate(val_data):
             data = gluon.utils.split_and_load(batch[0], ctx_list=ctx, batch_axis=0, even_split=False)
@@ -101,8 +97,6 @@ def validate(net, val_data, ctx, eval_metric, size, args):
 
 def demo_val(net, val_data, eval_metric, ctx, args):
     """Eval pipeline"""
-    net.collect_params().reset_ctx(ctx)
-
     # set up logger
     logging.basicConfig()
     logger = logging.getLogger()
@@ -113,8 +107,6 @@ def demo_val(net, val_data, eval_metric, ctx, args):
         os.makedirs(log_dir)
     fh = logging.FileHandler(log_file_path)
     logger.addHandler(fh)
-    mx.nd.waitall()
-    net.hybridize()
 
     map_bbox = validate(net, val_data, ctx, eval_metric, len(val_dataset), args)
     map_name, mean_ap = map_bbox
@@ -124,10 +116,8 @@ def demo_val(net, val_data, eval_metric, ctx, args):
 
 if __name__ == '__main__':
     args = parse_args()
-    # fix seed for mxnet, numpy and python builtin random generator.
-    gutils.random.seed(args.seed)
 
-    # training contexts
+    # evaluating contexts
     ctx = [mx.gpu(int(i)) for i in args.gpus.split(',') if i.strip()]
     ctx = ctx if ctx else [mx.cpu()]
 
@@ -135,18 +125,17 @@ if __name__ == '__main__':
     net_name = '_'.join(('yolo3', 'tiny_darknet', args.dataset))
     args.save_prefix += net_name
 
-    net = get_model(net_name, pretrained_base=True)
-    async_net = net
-    if args.resume.strip():
-        print(f'Loading {args.resume}')
-        net.load_parameters(args.resume.strip())
-        async_net.load_parameters(args.resume.strip())
-    else:
-        raise NotImplementedError("You haven't specify the prarms yet!")
+    net = get_model(net_name)
+    if not args.resume.strip():
+        if args.start_epoch == -1:
+            raise ValueError("You have to either give the path of the saved model or specify the start epoch!")
+        # Predict the path of the saved weights from the `start_epoch` parameter
+        args.resume = '{:s}_{:04d}.params'.format(args.save_prefix, args.start_epoch)
+    print(f'Loading weights from {args.resume}')
+    net.load_parameters(args.resume.strip())
 
     # val data
     val_dataset, eval_metric = get_dataset(args.dataset, args)
-    val_data = get_dataloader(
-        async_net, val_dataset, args.data_shape, args.batch_size, args.num_workers, args)
+    val_data = get_dataloader(val_dataset, args.data_shape, args.batch_size, args.num_workers, args)
     # Valing
     demo_val(net, val_data, eval_metric, ctx, args)
