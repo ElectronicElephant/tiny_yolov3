23b73ec395e24a4a497ed8887edeac628c2c77f2
3t <1489272013@qq.com>
2020-02-18 21:24:26 +0800

    Fix Bug

--- eval.py
+++ eval.py
@@ -86,7 +86,7 @@ def benchmark(net, val_data, ctx, size, args):
             pbar.update(batch[0].shape[0])
             pbar.set_description("Batch %.4f s | fps %.2f" % (inf_time, batch[0].shape[0] / inf_time))
 
-    print('Average fps %.2f' % size / total_time)
+    print('Average fps %.2f' % (size / total_time))
     return eval_metric.get()
 
 
